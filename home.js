a2bb83de70a000be70ae36c8765695986b14d516
rishi-novo <rishi.bajpai@novostack.com>
2022-08-18 18:04:51 +0530

    changed fonts and fixed ui bugs
    Applied default font as Minecrafter on whole website and fixed ui bugs like slider and logo section ::: responsiveness not covered

--- home.js
+++ home.js
@@ -1,25 +1,9 @@
-var document;
-
 let arrowTop = document.getElementById('top-arrow');
 let banner = document.getElementById('banner');
-let playBtn = document.getElementsById('play-btn');
-let imageContainer = document.getElementsById('image-container');
-let videoContainer = document.getElementsById('video-container');
-
-
-
 
 arrowTop.addEventListener("click", function(){
   alert('hi');
-    banner.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-  });
-
-
-playBtn.addEventListener("click", function () {
-  alert('hi');
-  imageContainer.classList.add('hide');
-  videoContainer.classList.remove('hide');
- 
+  banner.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 });
 
     
